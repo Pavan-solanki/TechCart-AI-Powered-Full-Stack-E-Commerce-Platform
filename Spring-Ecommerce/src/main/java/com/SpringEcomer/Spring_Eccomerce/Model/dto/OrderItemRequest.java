bc9d137efca2c3b24b152ea79ea9c07ccc7a74ca
Pavan Solanki <138639956+Pavan-solanki@users.noreply.github.com>
2025-11-04 10:46:55 +0530

    Delete Spring-Ecommerce directory

--- Spring-Ecommerce/src/main/java/com/SpringEcomer/Spring_Eccomerce/Model/dto/OrderItemRequest.java
+++
@@ -1,6 +0,0 @@
-package com.SpringEcomer.Spring_Eccomerce.Model.dto;
-
-public record OrderItemRequest(
-        int productId,
-        int quantity
-) {}
--- Spring-Ecommerce/src/main/java/com/SpringEcomer/Spring_Eccomerce/Model/dto/OrderItemResponse.java
+++
@@ -1,9 +0,0 @@
-package com.SpringEcomer.Spring_Eccomerce.Model.dto;
-
-import java.math.BigDecimal;
-
-public record OrderItemResponse(
-        String productName,
-        int quantity,
-        BigDecimal totalPrice
-) {}
